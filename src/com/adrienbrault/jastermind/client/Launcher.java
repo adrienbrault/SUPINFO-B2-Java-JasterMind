9c86737ae7760c978604b98d7fe124e03d97df7c
Adrien BRAULT <monsti@gmail.com>
2011-06-03 20:02:03 +0200

    Launchers

---
+++ src/com/adrienbrault/jastermind/client/Launcher.java
@@ -0,0 +1,15 @@
+package com.adrienbrault.jastermind.client;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @Author: adrienbrault
+ * @Date: 03/06/11 20:00
+ */
+public class Launcher {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
---
+++ src/com/adrienbrault/jastermind/server/Launcher.java
@@ -0,0 +1,15 @@
+package com.adrienbrault.jastermind.server;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @Author: adrienbrault
+ * @Date: 03/06/11 20:00
+ */
+public class Launcher {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
